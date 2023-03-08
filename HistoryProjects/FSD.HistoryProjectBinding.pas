c97dd86b22ce9ab0734eb58bacdf7aad9b10a043
mauricio.abreu@conectsolutionsti.com.br <Mauricio>
2023-03-08 15:39:34 -0300

    Reorganizado e criado opção do menu

---
+++ HistoryProjects/FSD.HistoryProjectBinding.pas
@@ -0,0 +1,19 @@
+unit FSD.HistoryProjectBinding;
+
+interface
+
+uses
+  ToolsAPI,
+  System.SysUtils;
+
+type TFSDHistoryProjectsBinding = class(TNOtifierObject, IOTAKeyboardBinding)
+
+  private
+
+  public
+
+end;
+
+implementation
+
+end.
